10024614aadf9b7a9cc789bd57141a702f13fcdc
bhytzer1 <davidemandricardo@tuta.com>
2026-04-22 00:22:39 +0200

    fatto un pochetto :0

--- python02/ex0/ft_first_exception.py
+++ python02/ex0/ft_first_exception.py
@@ -3,14 +3,16 @@ def input_temperature(temp_str: str) -> int:
     return int(temp_str)
 
 def test_temperature() -> None:
-    print(f"{input_temperature('25')}")
+    print(f"input data is '{input_temperature('25')}'")
+    print(f"Temperature is now {input_temperature('25')}°C\n")
+    print("Input data is 'abc'")
     try:
         input_temperature("abc")
     except ValueError as e:
-        print(f"{e}")
-    print("\n=== All tests completed, program didn't crash! ===")
+        print(f"Caught input_temperature error: {e}")
+    print("\nAll tests completed - program didn't crash!")
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     print("=== Garden Temperature ===\n")
-    print(f"Input data is '{input_temperature('25')'}")
+    test_temperature()
 
---
+++ python02/ex1/ft_raise_exception.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+def input_temperature(temp_str: str) -> int:
+    temp = int(temp_str)
+    if temp > 40:
+        raise ValueError(f"{temp}°C is too hot for plants (max 40°C)")
+    elif temp < 0
+        raise ValueError(f"{temp}°C is too cold for plants (min 0°C)")
+    else
+        return(temp)
+
+def test_temperature() -> None:
+    print(f"input data is '{input_temperature('25')}'")
+    print(f"Temperature is now {input_temperature('25')}°C\n")
+    print("Input data is 'abc'")
+    try:
+        input_temperature("abc")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    print("\nAll tests completed - program didn't crash!")
+
+def temperature_control(sensore: int) -> None:
+    if sensore > 40:
+        return(print(f"{sensore}°C is too hot for plants (max 40°C)"))
+    else if sensore < 0
+        return(print(f"{sensore}°C is too cold for plants (min 0°C)"))
+
