43c1299d7b17397c95e383aa94fa09507d2acbb7
bhytzer1 <davidemandricardo@tuta.com>
2026-04-22 22:43:05 +0200

    Add files via upload

---
+++ python02/ex3/ft_different_errors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+def garden_operations(operation_number: int) -> None:
+    if operation_number == 0:
+        int("abc")
+    elif operation_number == 1:
+        int("1 / 0")
+    elif operation_number == 2:
+        open("asdrubalino.txt")
+    elif operation_number == 3:
+        int("x" + 42)
+
+
+        except ValueError as e:
+            print(f"{e}")
+        except ZeroDivisionError as e:
+            print(f"{e}")
+        except FileNotFoundError as e:
+            print(f"{e}")
+        except TypeError as e:
+
